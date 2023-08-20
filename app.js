4a724e1ed2283ef8a45aebd9b53db4d7b3037653
Brayan Perafan <perafanbrayan1998@gmail.com>
2023-08-20 17:30:41 -0500

    c

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post("/webhook", (req, res) => {
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
      let msg_body1 = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-    socket.emit("getprods",msg_body1)
+    socket.emit("getprods1",msg_body1)
     let msg_body ="San Juan Electronics ";
       axios({
         method: "POST", // Required, HTTP method, a string, e.g. POST, GET
