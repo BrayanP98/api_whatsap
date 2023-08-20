fdd1de7d5570b683223920ca8b1a875da5d33fd1
Brayan Perafan <perafanbrayan1998@gmail.com>
2023-08-20 17:59:35 -0500

    new

--- app.js
+++ app.js
@@ -30,12 +30,12 @@ server.listen(PORT, () => {
 });
 io.on('connection', function(socket)  {
   socket.emit("getprods","bienvenidos todos")
-
+});
 
 app.post("/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
- 
+  io.emit('whatsapp_notification', body);
 
   // Check the Incoming webhook message
   //console.log(JSON.stringify(req.body, null, 2));
@@ -78,7 +78,7 @@ app.post("/webhook", (req, res) => {
   }
 });
 
-});
+
 app.get("/", (req, res) => {
   res.render("index.ejs")
 
