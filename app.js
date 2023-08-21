2dba4b800fb3837cfe4c40a3bbe5df3880f62454
Brayan Perafan <perafanbrayan1998@gmail.com>
2023-08-21 15:56:37 -0500

    s

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.post("/webhook", (req, res) => {
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
      var name=req.body.entry[0].changes[0].value.contacts[0].profile.name;
     
-     let msg_body1 = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      // extract the message text from the webhook payload
      
      if( req.body.entry[0].changes[0].value.messages[0].interactive.list_reply.description){
       let msg_interctive = req.body.entry[0].changes[0].value.messages[0].interactive.list_reply.description;
@@ -106,7 +106,7 @@ app.post("/webhook", (req, res) => {
 
      }else{
 
-
+      let msg_body1 = req.body.entry[0].changes[0].value.messages[0].text.body;
     
      
       console.log(msg_body1)
