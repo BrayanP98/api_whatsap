078f247517c6cf25429bf34c5007d74c9d55334f
Brayan Perafan <perafanbrayan1998@gmail.com>
2023-08-24 13:28:29 -0500

    c

--- app.js
+++ app.js
@@ -359,8 +359,8 @@ app.post("/webhook", (req, res) => {
         }else if(saludos.includes(msg_body1.toLowerCase())){
           let mesagge='de'+':'+ from +' '+msg_body1;
           io.emit('whatsapp_notification', mesagge);
-          let msg_body ="Hola "+"" +name+", "+"bienvenido a San Juan Electronics "+"\n Soy SecuriBot🤖  ¿Como puedo ayudarte?"+"\n\n1.Informacion CCTV"+
-          "\n2. Informacion GPS"+"\n3. Informacion Alarmas residenciales"+"\n4. Control de acceso"+"\n\n Escribe *ASESOR* si quieres comunicarte con uno de nuestros asesores"+"\n\nTu seguridad es nuestra prioridad!. \n\nEstamos ubicados en la transversal 9#57n-202 via al bosque."+
+          let msg_body ="Hola "+"" +name+", "+"bienvenido a San Juan Electronics. "+"\n Soy SecuriBot🤖  ¿Como puedo ayudarte?"+"\n\n1.Informacion CCTV."+
+          "\n2. Informacion GPS."+"\n3. Informacion Alarmas residenciales."+"\n4. Control de acceso."+"\n\n Escribe *ASESOR* si quieres comunicarte con uno de nuestros asesores"+"\n\nTu seguridad es nuestra prioridad!. \n\nEstamos ubicados en la transversal 9#57n-202 via al bosque."+
           "\n\n Siguenos en Facebook como San Juan Electronics."+"\n O visita nuestra WEB https://sanjuanelectronics.online/";          
           
           axios({
