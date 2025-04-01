82772ae3a7d0c9eaa322b163e6f993b85eab88c1
Brayan Perafan <perafanbrayan1998@gmail.com>
2025-04-01 01:09:50 +0000

    ds

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ async function generarRespuestaConAPI(pregunta) {
   try {
     // Realizar la solicitud a la API de Hugging Face
     const response = await axios.post(
-      'https://api-inference.huggingface.co/models/gpt2',
+      'https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium',
       { inputs: pregunta,
         parameters: { temperature: 0.7, max_length: 50,top_p: 0.9 } 
        },
